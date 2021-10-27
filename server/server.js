ea50b5ce8f3b4843e58e2c97d920a2961cedbe1a
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2021-10-27 14:35:57 +0200

    mongo connection is established

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const { Schema, model, connect, connection } = mongoose;
+const { connect, connection } = mongoose;
 
 const app = express();
+dotenv.config();
 
+connect(process.env.MONGO_URI);
 
+connection.on("connected", () => console.log("DB Connection established!"));
 
 const port = 3000;
 app.listen(port, () => console.log("Server running at %s.", port));
